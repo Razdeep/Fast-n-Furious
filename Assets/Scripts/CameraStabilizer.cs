ef36ae6aeb89b53feb866d5cbb5a9b71c829c95b
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2020-04-12 11:52:24 +0530

    Camera Stability Improved
     - Made the camera point to a invisible static box, this improved camera stability

---
+++ Assets/Scripts/CameraStabilizer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStabilizer : MonoBehaviour
+{
+
+    public GameObject car;
+    
+    void Update()
+    {
+        float y = car.transform.rotation.eulerAngles.y;
+        transform.eulerAngles = new Vector3(0, y, 0);
+    }
+}
